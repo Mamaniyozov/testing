42442e1049c405c2d2d2d7cb5404cb7dd9aa5aa4
Mmaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-06-26 23:04:54 +0500

    models is ready

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.admin import User
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-# Create your models here.
+    def __str__(self):
+        return self.title
+    
